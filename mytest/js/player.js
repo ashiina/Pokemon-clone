59d2123058c1bfb59ccff1e4b408856a6ac9a54c
Shohei Urano <s.runoa@gmail.com>
2015-07-12 23:30:58 +0900

    整理

--- mytest/js/player.js
+++ mytest/js/player.js
@@ -1,37 +1,41 @@
 
 var Player = function () {
-    this.animations = {};
     this.node;
     this.prev_key;
 
     this._contructor = function () {
         console.log("init player");
-        this.animations.player_idle = new $.gQ.Animation({imageURL:"http://veekun.com/static/pokedex/downloads/diamond-pearl-frame2.png"});
-        $("#actors").addSprite("player", {animation:this.animations.player_idle,
-            width:56, height:62, posx:STAGE_WIDTH/2, posy:STAGE_HEIGHT/2});
+        $("#actors").addSprite("player", {
+            width:56,
+            height:62,
+            posx:STAGE_WIDTH/2,
+            posy:STAGE_HEIGHT/2
+        });
         this.node = $("#player");
     };
 
     this.make_animation = function (key) {
         if (key == this.prev_key) return;
-        var type = '';
+        var type = 0;
         var offset_x = 0;
         var offset_y = 0;
-        if (key == KEY_NONE) {
+        if (key == KEY_NONE) { //キーが押されてなかった時は直前のキーに応じて画像を選択
             offset_x = 75;
             switch (this.prev_key) {
                 case KEY_A: offset_y = 0; break;
                 case KEY_D: offset_y = 75; break;
                 case KEY_W: offset_y = 150; break;
-                case KEY_S: offset_y = 225; break;
+                case KEY_S:
+                default: offset_y = 225; break;
             }
-        } else {
+        } else { //キーに応じてアニメーション
             type = $.gQ.ANIMATION_PINGPONG | $.gQ.ANIMATION_HORIZONTAL;
             switch (key) {
                 case KEY_A: offset_y = 0; break;
                 case KEY_D: offset_y = 75; break;
                 case KEY_W: offset_y = 150; break;
-                case KEY_S: offset_y = 225; break;
+                case KEY_S:
+                default: offset_y = 225; break;
             }
         }
         var animation = new $.gQ.Animation({
@@ -59,8 +63,8 @@ var Player = function () {
         this.prev_key = key;
 
         if (newx < 0) newx = 0;
-        if (newx > STAGE_WIDTH - 56) newx = STAGE_WIDTH-56;
-        if (newy > STAGE_HEIGHT - 62) newy = STAGE_HEIGHT-62;
+        if (newx > STAGE_WIDTH - 56) newx = STAGE_WIDTH - 56;
+        if (newy > STAGE_HEIGHT - 62) newy = STAGE_HEIGHT - 62;
         if (newy < 0) newy = 0;
 
         var c = $("#player").collision("#objects,.object_1", {x:newx, y:newy});
